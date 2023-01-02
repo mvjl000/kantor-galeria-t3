3caf30c4e02242a1bb168210dda607481aa11c5c
Miłosz <milosz.devmail@gmail.com>
2023-01-02 17:34:22 +0100

    Setup prisma and add back trpc procedures

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,8 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  // const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
+  const currencies = trpc.currencies.getCurrencies.useQuery();
+  console.log("CURRENCIES", currencies);
 
   return (
     <>
