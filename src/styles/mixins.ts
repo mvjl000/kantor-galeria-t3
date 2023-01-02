9568ce14c634659cc5dddd1261357c64c5c9f5d6
Miłosz <milosz.devmail@gmail.com>
2023-01-02 17:49:32 +0100

    Add global styles & theme & mixins

---
+++ src/styles/mixins.ts
@@ -0,0 +1,33 @@
+import { css } from "@emotion/react";
+
+export const flexBetween = css`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const flexCenter = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const flexLeft = css`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+export const flexColumnBetween = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const flexColumnCenter = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
---
+++ src/styles/theme.ts
@@ -0,0 +1,46 @@
+import { Theme } from "@emotion/react";
+
+export const theme: Theme = {
+  colors: {
+    white: "hsl(0, 0%, 100%)",
+    black: "hsl(0, 0%, 0%)",
+    grey: "hsl(0, 0%, 80%)",
+    lightGrey: "hsl(0, 0%, 95%)",
+    red: "hsl(0, 100%, 60%)",
+  },
+  font: {
+    size: {
+      xSmall: "1.5rem",
+      small: "1.8rem",
+      medium: "2.4rem",
+      large: "3rem",
+      xLarge: "3.5rem",
+      huge: "4rem",
+    },
+    color: {
+      black: "hsl(0, 0%, 0%)",
+      grey: "hsl(0, 0%, 40%)",
+    },
+    family: {
+      roboto: '"Roboto Condensed", sans-serif',
+      josefin: '"Josefin Sans", sans-serif',
+    },
+    weight: {
+      light: 300,
+      medium: 400,
+      bold: 600,
+    },
+  },
+  mq: {
+    tablet: "@media (min-width: 768px)",
+    desktop: "@media (min-width: 1024px)",
+    bigDesktop: "@media (min-width: 1280px)",
+    huge: "@media (min-width: 1440px)",
+  },
+  zIndex: {
+    laoder: 2,
+    modal: 10,
+    menu: 100,
+    burger: 101,
+  },
+};
