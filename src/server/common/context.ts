5a49e9ada22393f06909c4825c012a0f90af6d27
Miłosz <milosz.devmail@gmail.com>
2023-01-02 15:05:45 +0100

    Add trpc context file

---
+++ src/server/common/context.ts
@@ -0,0 +1,20 @@
+import { type inferAsyncReturnType } from "@trpc/server";
+import * as trpcNext from "@trpc/server/adapters/next";
+import { getSession as getSessionAuth0 } from "@auth0/nextjs-auth0";
+
+// Function which provides context to all procedures. Runs with every request.
+export async function createContext({
+  req,
+  res,
+}: trpcNext.CreateNextContextOptions) {
+  const getSession = () => {
+    const session = getSessionAuth0(req, res);
+    return session;
+  };
+
+  return {
+    session: getSession(),
+  };
+}
+
+export type TrpcContext = inferAsyncReturnType<typeof createContext>;
