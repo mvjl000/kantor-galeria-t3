eb403cfd24f01e6924a3775c92cc59fe2fff48ea
Miłosz <milosz.devmail@gmail.com>
2023-01-09 16:28:33 +0100

    Add contact & 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { H1 } from "../components/ui";
+
+const Page404: NextPage = () => {
+  return (
+    <div>
+      <H1>404</H1>
+    </div>
+  );
+};
+
+export default Page404;
---
+++ src/pages/kontakt.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { H1 } from "../components/ui";
+
+const Contact: NextPage = () => {
+  return (
+    <div>
+      <H1>Contact Page</H1>
+    </div>
+  );
+};
+
+export default Contact;
