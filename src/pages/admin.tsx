dba776f0ee7b6cda0bc94fab57a311d72fd1e534
Miłosz <milosz.devmail@gmail.com>
2023-01-09 17:33:49 +0100

    Create Admin page

---
+++ src/pages/admin.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { H1 } from "../components/ui";
+
+const Admin: NextPage = () => {
+  return (
+    <>
+      <H1>Panel Administratora</H1>
+    </>
+  );
+};
+
+export default Admin;
